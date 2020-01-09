efa86e5395f191e300c487114312b20da096a1f8
‍肖宇 <304491159@qq.com>
2020-01-09 16:57:51 +0800

    下单

---
+++ src/main/java/club/mrxiao/kdniao/api/impl/KdniaoServiceImpl.java
@@ -0,0 +1,14 @@
+package club.mrxiao.kdniao.api.impl;
+
+
+import club.mrxiao.kdniao.api.KdniaoService;
+
+/**
+ * <pre>
+ *     快递鸟api接口实现
+ * </pre>
+ * @author <a href="https://github.com/mr-xiaoyu">xiaoyu</a>
+ * @since 2020-01-09
+ */
+public class KdniaoServiceImpl implements KdniaoService {
+}
